6de83de97ce3603814ba3f46ce5454103b1dc549
Adil Emre <adil.emre@hotmail.com>
2026-09-18 14:03:34 +0300

    Initial Commit

---
+++ Gardropp/GardroppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardroppApp.swift
+//  Gardropp
+//
+//  Created by adil emre  on 18.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct GardroppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
